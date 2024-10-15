5f65fef170036396eb6f4cc4ca94bfb110149f17
taha dawood <71606996+tahamoh33@users.noreply.github.com>
2024-10-15 13:39:30 +0300

    Add files via upload

---
+++ kheap.c
@@ -0,0 +1,69 @@
+#include <inc/memlayout.h>
+#include <kern/kheap.h>
+#include <kern/memory_manager.h>
+
+//2022: NOTE: All kernel heap allocations are multiples of PAGE_SIZE (4KB)
+
+
+void* kmalloc(unsigned int size)
+{
+	//TODO: [PROJECT 2022 - [1] Kernel Heap] kmalloc()
+	// Write your code here, remove the panic and write your code
+	kpanic_into_prompt("kmalloc() is not implemented yet...!!");
+
+	//NOTE: Allocation using NEXTFIT strategy
+	//NOTE: All kernel heap allocations are multiples of PAGE_SIZE (4KB)
+	//refer to the project presentation and documentation for details
+
+
+	//TODO: [PROJECT 2022 - BONUS1] Implement a Kernel allocation strategy
+	// Instead of the Next allocation/deallocation, implement
+	// BEST FIT strategy
+	// use "isKHeapPlacementStrategyBESTFIT() ..."
+	// and "isKHeapPlacementStrategyNEXTFIT() ..."
+	//functions to check the current strategy
+	//change this "return" according to your answer
+
+	return NULL;
+
+
+}
+
+void kfree(void* virtual_address)
+{
+	//TODO: [PROJECT 2022 - [2] Kernel Heap] kfree()
+	// Write your code here, remove the panic and write your code
+	panic("kfree() is not implemented yet...!!");
+
+	//you need to get the size of the given allocation using its address
+	//refer to the project presentation and documentation for details
+
+}
+
+unsigned int kheap_virtual_address(unsigned int physical_address)
+{
+	//TODO: [PROJECT 2022 - [3] Kernel Heap] kheap_virtual_address()
+	// Write your code here, remove the panic and write your code
+	panic("kheap_virtual_address() is not implemented yet...!!");
+
+	//return the virtual address corresponding to given physical_address
+	//refer to the project presentation and documentation for details
+
+	//change this "return" according to your answer
+
+	return 0;
+}
+
+unsigned int kheap_physical_address(unsigned int virtual_address)
+{
+	//TODO: [PROJECT 2022 - [4] Kernel Heap] kheap_physical_address()
+	// Write your code here, remove the panic and write your code
+	panic("kheap_physical_address() is not implemented yet...!!");
+
+	//return the physical address corresponding to given virtual_address
+	//refer to the project presentation and documentation for details
+
+	//change this "return" according to your answer
+	return 0;
+}
+
